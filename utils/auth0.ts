3a614f1f9a20372b241b1f1662442126f817e298
robert <dungeonry@hotmail.com>
2020-10-15 15:07:25 -0700

    Added environment toggle for /utils/auth0.ts to change roots between localhost and the production root url.  Removed dotenv because it seems vercel doesn't need it.

--- utils/auth0.ts
+++ utils/auth0.ts
@@ -1,13 +1,19 @@
 import { initAuth0 } from '@auth0/nextjs-auth0';
-require("dotenv").config();
+
+const { NODE_ENV } = process.env;
+
+const ROOT_URL = NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://fifis.now.sh"
+
 
 export default initAuth0({
     domain: process.env.AUTH0_DOMAIN,
     clientId: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     scope: 'openid profile',
-    redirectUri: 'http://localhost:3000/api/callback',
-    postLogoutRedirectUri: 'http://localhost:3000/',
+    redirectUri: `${ROOT_URL}/api/callback`,
+    postLogoutRedirectUri: `${ROOT_URL}/`,
     session: {
         cookieSecret: process.env.COOKIE_SECRET,
         cookieLifetime: 60 * 60 * 8,
